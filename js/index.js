4419c15d84d707597c9249d1fbd5335ec9fa6e81
柳絮 <15153447169@163.com>
2018-07-10 01:25:28 +0800

    demo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,64 @@
 $(function (){
 
+//卡片事件
+
+    var key=true
+
+    var guoqusuoyin
+
+
+     $.each($('#gerenjieshao .rongqi'),function(i,ele){
+
+            $(ele).on('click',function(){
+
+                if(key){
+
+                    $(this).children('.text').css({
+                        transform:'rotateX(0deg)'
+                    })
+
+                    $(this).parent().siblings().find('.text').css({
+                        transform:'rotateX(90deg)'
+                    })
+
+                    key=false
+
+                    guoqusuoyin=$(this).parent().index()
+
+                }else{
+
+                    if($(this).parent().index()==guoqusuoyin){
+
+                        $(this).children('.text').css({
+                            transform:'rotateX(90deg)'
+                        })
+
+                        key=true
+                    }else{
+
+                        $(this).children('.text').css({
+                            transform:'rotateX(0deg)'
+                        })
+
+                        $(this).parent().siblings().find('.text').css({
+                            transform:'rotateX(90deg)'
+                        })
+
+                        key=false
+
+                        guoqusuoyin=$(this).parent().index()
+                    }
+
+                }
+
+
+
+            })
+
+     })
+
+
+
 
 
 })
