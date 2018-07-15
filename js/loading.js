67441e9036ee67a13d2ac7fbc9bb872f0ed5f729
柳絮 <15153447169@163.com>
2018-07-15 16:25:22 +0800

    demo

--- js/loading.js
+++ js/loading.js
@@ -30,7 +30,7 @@
 
             imgObj.src=src
 
-            $(imgObj).on('load error',function(){
+            $(imgObj).on('load',function(){
                     count++
                          opt.each && opt.each(count,len)
                 if(count==len){
